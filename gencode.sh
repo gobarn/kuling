9d60ac1ea955840abb982fdd5343912fe572678c
Fredrik Bäckström <fredrik.backstrom@izettle.com>
2015-07-02 23:27:30 +0200

    removed gencode

--- gencode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/projects/golang/src/github.com/golang/protobuf
-
-protoc  -I ../../fredrikbackstrom/kuling/kuling/broker ../../fredrikbackstrom/kuling/kuling/broker/broker.proto --go_out=plugins=grpc:../../fredrikbackstrom/kuling/kuling/broker
-
-protoc  -I ../../fredrikbackstrom/kuling/kuling ../../fredrikbackstrom/kuling/kuling/commandserver.proto --go_out=plugins=grpc:../../fredrikbackstrom/kuling/kuling
